1fec68c7a4114a2937b22d710da4d3384531b55d
+ <+>
2025-11-25 09:53:21 +0300

    p3-organizing-bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 25, 'M', 'brown', 'calm', 1),
+('Rowdy', 8, 'M', 'black', 'aggressive', 1),
+('Tabitha', 12, 'F', 'brown', 'friendly', 1),
+('Sergeant Brown', 15, 'M', 'brown', 'strict', 1),
+('Melissa', 10, 'F', 'white', 'gentle', 1),
+('Grinch', 5, 'M', 'green', 'grumpy', 1),
+('Wendy', 7, 'F', 'blonde', 'cheerful', 1),
+(NULL, 3, 'M', 'gray', 'mysterious', 1);
